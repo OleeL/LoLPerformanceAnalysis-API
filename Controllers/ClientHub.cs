1b521934594d44ed8538b7886234f8ab577ad493
OleeL <oliglegg@hotmail.co.uk>
2020-08-20 22:36:48 +0100

    Code cleanup

--- Controllers/ClientHub.cs
+++ Controllers/ClientHub.cs
@@ -29,20 +29,32 @@ namespace LoLPerformanceAnalysisAPI.Controllers
         #endregion
 
         #region [ClientGets]
+        // Gets all information of a summoner. Stores it locally to prevent spam
         public async Task<Summoner> GetSummoner(string summonerName, string serverRegion){
+            // Input validation and prevents multiple requests for one summoner
             if (summonerName == null || summonerName == "") return null;
             var summoner = TryGetSummoner(Context.ConnectionId);
             if (summoner != null || summoner?.name == summonerName) return summoner;
             summoner = JsonConvert.DeserializeObject<Summoner>(await Requests.GetSummonerId(summonerName, serverRegion));
-            summoner.leagueEntry = JsonConvert.DeserializeObject<List<LeagueEntryDTO>>(await Requests.GetSummonerLeagueEntry(summoner.id, serverRegion));
-            summoner.matchList = JsonConvert.DeserializeObject<MatchlistDTO>(await Requests.GetMatchHistory(summoner.accountId, serverRegion));
+
+            // Getting league entry
+            var leagueEntry = await Requests.GetSummonerLeagueEntry(summoner.id, serverRegion);
+            summoner.leagueEntry = JsonConvert.DeserializeObject<List<LeagueEntryDTO>>(leagueEntry);
+
+            // Getting match list
+            var matchList = await Requests.GetMatchHistory(summoner.accountId, serverRegion);
+            summoner.matchList = JsonConvert.DeserializeObject<MatchlistDTO>(matchList);
+
+            // Stores the summoner locally
             TryStoreSummoner(Context.ConnectionId, summoner);
             return summoner;
         }
 
+        // Gets champion details
         public async Task<string> GetChampionDetails(string champion) =>
             await Requests.GetChampion(champion);
     
+        // Gets champion rotations
         public async Task<string> GetChampionRotations(string platform) =>
             await Requests.GetChampionRotations(platform);
         
